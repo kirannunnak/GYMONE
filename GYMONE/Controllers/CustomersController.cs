7f7be589a0dfc96a149efdf7476af6e247b160d0
devops <kirannunnak@gmail.com>
2025-02-11 17:06:14 +0530

    Add project files.

---
+++ GYMONE/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GYMONE.Models;
+using GYMONE.Repository;
+
+namespace GYMONE.Controllers
+{
+    public class CustomersController : Controller
+    {
+        //
+        // GET: /CustomersController.cs/
+        IPaymentlisting objIPaymentlisting;
+
+        public PaymentController()
+        {
+            objIPaymentlisting = new Paymentlisting();
+        }
+
+
+        public ActionResult CustomerDetails(PaymentlistingDTOVM model, string SearchButton)
+        {
+            return View(model);
+        }
+
+    }
+}
